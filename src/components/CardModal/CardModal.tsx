c1ef8ac54ce3b34034605a5b4008631b9cff3361
Christian Diaz Portela <christiandp7@gmail.com>
2021-09-15 12:24:57 -0400

    :sparkles: PDF Export done!!!

--- src/components/CardModal/CardModal.tsx
+++ src/components/CardModal/CardModal.tsx
@@ -1,9 +1,15 @@
 import React, { FC, useState, useContext } from 'react'
+import cn from 'classnames'
 import s from './CardModal.module.css'
 import Modal from 'react-modal'
 import { CardContext } from '@context/CardContext'
 import type { CardData } from '@context/CardContext'
 import { BsX } from 'react-icons/bs'
+import { motion } from 'framer-motion'
+// import ReactPDF from '@react-pdf/renderer'
+import CardTemplate from '@components/CardTemplate'
+import jsPDF from 'jspdf'
+import { Scrollbars } from 'react-custom-scrollbars-2'
 
 export interface CardModalData {
 	open: boolean
@@ -22,6 +28,10 @@ const CardModal: FC<Props> = ({ modal, setModal }) => {
 		setModal({ open: false, place: '' })
 	}
 
+	const handlePDF = () => {
+		// ReactPDF.render(<CardTemplate />, 'example.pdf')
+	}
+
 	return (
 		<Modal isOpen={modal.open} className={s.modal} overlayClassName={s.overlay}>
 			<div className={s.modalHeader}>
@@ -29,8 +39,12 @@ const CardModal: FC<Props> = ({ modal, setModal }) => {
 					<BsX />
 				</button>
 			</div>
-			<div className={s.modalBody}>{modal.place}</div>
-			<div className={s.modalFooter}></div>
+			<div className={s.modalBody}>
+				<CardTemplate place={modal.place} closeModal={closeModal} />
+			</div>
+			<div className={s.modalFooter}>
+				{/* <button onClick={handlePDF}>render pdf</button> */}
+			</div>
 		</Modal>
 	)
 }
---
+++ src/components/CardTemplate/CardTemplate.tsx
@@ -0,0 +1,123 @@
+import React, { FC, useRef } from 'react'
+import s from './CardTemplate.module.css'
+import Pdf from 'react-to-pdf'
+import Template from '@components/svg/Template'
+import { useMediaQuery } from 'react-responsive'
+
+interface Props {
+	place: string
+	closeModal: () => void
+}
+
+const CardTemplate: FC<Props> = ({ place, closeModal }) => {
+	const ref = useRef(null)
+
+	const isMd = useMediaQuery({ query: '(min-width: 768px)' })
+	const isLg = useMediaQuery({ query: '(min-width: 1024px)' })
+
+	const sizes = {
+		lg: {
+			//.75
+			width: 527.25,
+			height: 620.25,
+			scale: 1.25,
+		},
+		md: {
+			//.5
+			width: 351.5,
+			height: 413.5,
+			scale: 1.5,
+		},
+		default: {
+			// .35
+			width: 246.05,
+			height: 289.45,
+			scale: 1.75,
+		},
+	}
+
+	const widthK = (): number => {
+		if (isLg) {
+			return sizes.lg.width
+		}
+		if (isMd) {
+			return sizes.md.width
+		}
+		return sizes.default.width
+	}
+	const heightK = (): number => {
+		if (isLg) {
+			return sizes.lg.height
+		}
+		if (isMd) {
+			return sizes.lg.height
+		}
+		return sizes.default.height
+	}
+	const fontK = (): number => {
+		if (isLg) {
+			return 80
+		}
+		if (isMd) {
+			return 50
+		}
+		return 35
+	}
+	const scaleK = (): number => {
+		if (isLg) {
+			return sizes.lg.scale
+		}
+		if (isMd) {
+			return sizes.md.scale
+		}
+		return sizes.default.scale
+	}
+
+	return (
+		<>
+			<div className={s.doc}>
+				<div ref={ref} className={s.wrapper}>
+					<Template width={widthK()} height={heightK()} className={s.template} />
+					<div className={s.placeContainer}>
+						<div
+							className={s.placeText}
+							style={{
+								fontSize: `${fontK() / place.length}rem`,
+							}}>
+							{place}
+						</div>
+					</div>
+				</div>
+				<Pdf
+					targetRef={ref}
+					filename="card.pdf"
+					scale={scaleK() * 1.25}
+					x={80}
+					y={80}
+					onComplete={closeModal}
+					options={{ unit: 'px' }}>
+					{({ toPdf }: any) => (
+						<div className={s.printContainer}>
+							<ul>
+								<li>
+									<button onClick={toPdf}>enprime</button>
+								</li>
+								<li>
+									<button onClick={toPdf}>imprimir</button>
+								</li>
+								<li>
+									<button onClick={toPdf}>imprimer</button>
+								</li>
+								<li>
+									<button onClick={toPdf}>print</button>
+								</li>
+							</ul>
+						</div>
+					)}
+				</Pdf>
+			</div>
+		</>
+	)
+}
+
+export default CardTemplate
