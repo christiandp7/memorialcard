5945230405dfcbc19f63a7e50da26110b7c9e978
Christian Diaz Portela <christiandp7@gmail.com>
2021-09-16 11:42:39 -0400

    :sparkles: Update about and add favicon

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -8,24 +8,15 @@ const About: FC = () => {
 		<Layout about>
 			<div className={s.rte}>
 				<p>
-					Memoria en reversa documenta la demográfica de la Reserva Jackie Onassis en
-					el Parque Central de Nueva York a traves de unas tarjetas de recuerdo que
-					apela a la construcción de la memoria y comunidad.
-				</p>
-				<p>
-					La idea no es crear un recuerdo de la ciuda de Nueva York, sino de donde
-					venimos y donde estamos. Para los turistas es un volver a un pasado
-					cercano, a los trabajadores o residentes los lleva a un pasado de 30, 25,
-					15 o los años que fueran hacia atrás, a sus orígenes, a su familia.
-				</p>
-				<p>
-					No son de la ciudad de Nueva York sino de otras ciudades o países de donde
-					podrían venir los trabajadores o turistas del area.
-				</p>
-				<p>
 					Memoria en reversa, una acción documenta la demográfica de la Reserva
-					Jackie Onassis en el Parque Central de Nueva York a traves de unas tarjetas
+					Jackie Onassis en el Parque Central de Nueva York a través de unas tarjetas
 					de recuerdo que apela a la construcción de la memoria y comunidad.
+				</p>
+				<p>
+					Las tarjetas no crean un recuerdo solo de la ciudad de Nueva York, sino de
+					donde venimos y donde estamos. Para los turistas es un volver a un pasado
+					cercano, para a los trabajadores o residentes los lleva a un pasado de 30,
+					25, 15 o los años que fueran hacia atrás, a sus orígenes, a su familia.
 				</p>
 			</div>
 		</Layout>
